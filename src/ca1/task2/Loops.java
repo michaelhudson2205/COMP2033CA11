6c3ced433ad5af95b9525dbdbc10cb9b7357d8dd
Michael Hudson <michaelhudson2205@gmail.com>
2024-04-16 20:35:04 +1000

    Completed task 2.

--- src/ca1/task2/Loops.java
+++ src/ca1/task2/Loops.java
@@ -1,7 +1,18 @@
 package ca1.task2;
 
-public class Loops {
-	public void printWords(int numberOfTimes) {
-		
+public class Loops 
+{
+	public static void main(String[] args)
+	{
+		Loops myLoops = new Loops();
+		myLoops.printWords(5);
+	}
+	
+	public void printWords(int numberOfTimes) 
+	{
+		for (int i = 0; i < numberOfTimes; i++)
+		{
+			System.out.println("words");
+		}
 	}
 }
