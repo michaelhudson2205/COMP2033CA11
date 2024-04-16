b18f142410e9d6b640d8a7694c0ef5613dc1081a
Michael Hudson <michaelhudson2205@gmail.com>
2024-04-17 07:40:59 +1000

    Completed count method. Still need unit tests.

--- src/ca1/task3/ArrayCount.java
+++ src/ca1/task3/ArrayCount.java
@@ -1,7 +1,24 @@
 package ca1.task3;
 
-public class ArrayCount {
-	public int count(String[] array, String target) {
-		return 0;
-	}
-}
+public class ArrayCount 
+{
+	public static void main(String[] args)
+	{
+		// Driver code for testing (**not unit testing**)
+		String[] arr1 = {"Apple", "Banana", "Orange", "Apple", "Pear", "Orange", "Pear", "Pear"};
+		System.out.println(count(arr1, "Grape"));
+	} //==========end of psvm==========
+	
+	public static int count(String[] array, String target) 
+	{
+		int res = 0;
+		for (int i = 0; i < array.length; i++)
+		{
+			if (target.equals(array[i]))
+			{
+				res++;
+			}
+		}
+		return res;
+	} // ==========end of count==========
+} // ==========end of class ArrayCount==========
